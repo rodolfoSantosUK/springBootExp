d3fa44e404ba1968c6659d985ffde57dd1c571be
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-10-05 19:29:05 -0300

    Commit do arquivo de properties e da atação development.

--- VendaSBoot/src/main/java/br/com/github/br/com/github/demo/MinhaConfiguration.java
+++ VendaSBoot/src/main/java/br/com/github/br/com/github/demo/MinhaConfiguration.java
@@ -1,13 +1,21 @@
 package br.com.github.br.com.github.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 
-@org.springframework.context.annotation.Configuration
+
 public class MinhaConfiguration {
 
     @Bean(name = "applicationName")
     public String applicationName() {
         return "applicationName";
+    }
+
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("RODANDO A CONFIGURACAO DE DESENVOLVIMENTO " );
+        };
     }
 
 }
--- VendaSBoot/src/main/java/br/com/github/br/com/github/demo/VendasApplication.java
+++ VendaSBoot/src/main/java/br/com/github/br/com/github/demo/VendasApplication.java
@@ -2,6 +2,7 @@ package br.com.github.br.com.github.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
+//    @Autowired
+//    @Qualifier("applicationName")
+    @Value("${application.name}")
     private String applicationName;
 
     @GetMapping("/hello")
